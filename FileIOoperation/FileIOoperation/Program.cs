c25d26855249ca5a2913296068bc6ee73ba33754
Praan26 <arpanap102@gmail.com>
2022-05-22 19:43:16 +0530

    [Arpan] Add: File input and output operation added

---
+++ FileIOoperation/FileIOoperation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FileIOoperation;
+class program
+{
+    public static void Main(string[] args)
+    {
+        OperationIO operationIO = new OperationIO();
+        operationIO.FileExists();
+        operationIO.ReadAllLines();
+        operationIO.ReadAllText();
+        operationIO.CopyFile();
+        operationIO.DeleteFilePath();
+        operationIO.UsingStreamReader();
+    }
+}
